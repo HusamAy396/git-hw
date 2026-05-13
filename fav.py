3d757d9687deb956689fef9dcb874a919130ef73
HusamAy396 <hus20230878@std.psut.edu.jo>
2026-05-13 22:43:30 +0300

    Add favorite Python program

---
+++ fav.py
@@ -0,0 +1,6 @@
+favorite_food = "mansaf"
+favorite_movie = "Interstellar"
+name = "Husam Ayman "
+print("My Name Is:", name)
+print("Favorite food:", favorite_food)
+print("Favorite movie:", favorite_movie)
